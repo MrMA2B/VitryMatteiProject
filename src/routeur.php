37cb8c44c2ac48e3703d6c9e12800722d116e776
Adrien Mattei <l3miagecours@gmail.com>
2021-11-30 10:02:32 +0100

    ajout de code

---
+++ src/routeur.php
@@ -0,0 +1,33 @@
+<?php
+
+/* namespace App;
+
+use App\controller\PostController;
+
+class Router
+{
+    public function run()
+    {
+        $route = $_GET['route'] ?? null;
+        $action = $_GET['action'] ?? null;
+
+        if (isset($_GET['route'])) {
+            if ('post' === $route && $action) {
+                $postController = new PostController();
+
+                if ('create' === $action) {
+                    return $postController->create();
+                } elseif ('read' === $action && isset($_GET['id'])) {
+                    var_dump($_GET['id']);die;
+                    return $postController->read($_GET['id']);
+                }
+            } elseif ('contact' === $route) {
+                var_dump('contact');
+            }
+        } else {
+            var_dump('hello');
+            require_once 'index.php';
+        }
+    }
+}
+*/
